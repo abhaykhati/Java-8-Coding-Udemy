b5f262e49570b673d351717f55243556e1b2d06f
Abhay Khati <abhaykhati@gmail.com>
2019-09-27 18:01:39 +0530

    BiPredicate with custom Employee object added

---
+++ src/java_8_code/functionalInterfaces/BiPredicateEmployeeExample.java
@@ -0,0 +1,83 @@
+package java_8_code.functionalInterfaces;
+
+import java.util.function.BiPredicate;
+
+public class BiPredicateEmployeeExample {
+	
+	public static void main(String[] args){ 
+		 
+		   Employee emp = new Employee(1,"Robert",35,"Manager");
+		 
+		   BiPredicate<Employee,String> biPredicate1 = ( x , y ) -> {
+		                                                             if( x.getDesignation().equalsIgnoreCase(y)){
+		                                                                 return true;
+		                                                             }else{
+		                                                                 return false;
+		                                                             }};
+		   // Example of test method
+		   boolean result = biPredicate1.test(emp,"Manager");
+		   System.out.println(result);
+		 
+		   // Example of negate method
+		   boolean result3 = (biPredicate1.negate()).test(emp,"Manager");
+		   System.out.println(result3);
+		 
+		   BiPredicate<Employee,String> biPredicate2 = ( x , y ) -> {
+		                                                             if( x.getDesignation().equalsIgnoreCase(y)){
+		                                                                 return true;
+		                                                             }else{
+		                                                                 return false;
+		                                                             }};
+		 
+		   // Example of and method
+		   boolean result1 = biPredicate1.and(biPredicate2).test(emp,"Manager");
+		   System.out.println(result1);
+		 
+		   // Example of or method
+		   boolean result2 = biPredicate1.or(biPredicate2).test(emp,"General Manager");
+		   System.out.println(result2);
+		 
+		}    	
+
+}
+class Employee {
+	 
+	   private long empId;
+	   private String name;
+	   private int age;
+	   private String designation;
+	 
+	   public Employee(long empId, String name, int age, String designation) {
+	     super();
+	     this.empId = empId;
+	     this.name = name;
+	     this.age = age;
+	     this.designation = designation;
+	   }
+	 
+	   public long getEmpId() {
+	     return empId;
+	   }
+	   public void setEmpId(long empId) {
+	     this.empId = empId;
+	   }
+	   public String getName() {
+	     return name;
+	   }
+	   public void setName(String name) {
+	     this.name = name;
+	   }
+	   public int getAge() {
+	     return age;
+	   }
+	   public void setAge(int age) {
+	     this.age = age;
+	   }
+	   public String getDesignation() {
+	     return designation;
+	   }
+	   public void setDesignation(String designation) {
+	     this.designation = designation;
+	   }
+	 
+	}
