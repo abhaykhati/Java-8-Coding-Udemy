a72081cef0ca64bea1b586b82a4683b69bf17ead
Abhay Khati <abhaykhati@gmail.com>
2019-09-30 15:22:52 +0530

    minor changes to FuctionalInterfaceExample

--- src/java_8_code/functionalInterfaces/FuctionalInterfaceExample.java
+++ src/java_8_code/functionalInterfaces/FuctionalInterfaceExample.java
@@ -5,13 +5,13 @@ import java.util.function.Function;
 public class FuctionalInterfaceExample {
 
 	static Function<String, String> firstFunction=(name) -> name.toUpperCase();
-	static Function<String, String> andThenFunction=(name)-> name.toUpperCase().concat(" welcome");
+	static Function<String, String> andThenFunction=(name)-> name.toUpperCase().concat("abhay ");
 	
 	public static void main(String[] args) {
 		
-		System.out.println("First Function=  "+ firstFunction.apply("abhay"));
-		System.out.println("andThen Function=  "+firstFunction.andThen(andThenFunction).apply("abhay"));
-		System.out.println("compose Function=  "+firstFunction.compose(andThenFunction).apply("abhay"));
+		System.out.println("First Function=  "+ firstFunction.apply("abhay")); // prints ABHAY
+		System.out.println("andThen Function=  "+firstFunction.andThen(andThenFunction).apply(" welcome"));// prints welcomeabhay
+		System.out.println("compose Function=  "+firstFunction.compose(andThenFunction).apply(" welcome")); // prints WELCOMEABHAY
 
 	}
 
