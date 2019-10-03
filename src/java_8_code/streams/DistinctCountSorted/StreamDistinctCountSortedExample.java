dbb597ea8464ebe8f7f435cd452277a30edede98
Abhay Khati <abhaykhati@gmail.com>
2019-10-03 22:49:07 +0530

    Stream Distinct(),Count(),Sorted() Example added

---
+++ src/java_8_code/streams/DistinctCountSorted/StreamDistinctCountSortedExample.java
@@ -0,0 +1,60 @@
+package java_8_code.streams.DistinctCountSorted;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java_8_code.data.Student;
+import java_8_code.data.StudentDataBase;
+
+public class StreamDistinctCountSortedExample {
+
+	public static long printDistinctCountSortedStudentActivities() {
+
+		long countOfStudentActivities = StudentDataBase.getAllStudents()
+										.stream().map(Student::getActivities)
+										.flatMap(List::stream)
+										.distinct()
+										.sorted()
+										.count();
+
+		return countOfStudentActivities;
+	}
+
+	public static List<String> printDistinctSortedStudentActivities() {
+
+		List<String> listOfStudentActivities = StudentDataBase.getAllStudents()
+										.stream()
+										.map(Student::getActivities)
+										.flatMap(List::stream)
+										.distinct()
+										.sorted()
+										.collect(Collectors.toList());
+
+		return listOfStudentActivities;
+	}
+
+	public static void main(String[] args) {
+    
+		
+		System.out.println("count of student activities=  " + printDistinctCountSortedStudentActivities());
+		
+		System.out.println();
+		List<String> printDistinctSortedStudentActivitiesList = printDistinctSortedStudentActivities();
+		
+		/*
+		 * 	int count=0;
+			for (String activities : printDistinctSortedStudentActivitiesList) {
+			System.out.print(++count +"  ");
+			System.out.println("Activities=  "+activities.toUpperCase());
+			//System.out.println((++count)+" Activities=  "+ activities);
+			
+		}*/
+		
+		
+		printDistinctSortedStudentActivitiesList.forEach(act -> System.out.println("  Student Activity= "+act));
+		
+		
+
+	}
+
+}
