ea39be7ac3d6be27b7c7067788c7973777fd1107
Abhay Khati <abhaykhati@gmail.com>
2019-10-11 12:52:18 +0530

    minor changes to student database

--- src/java_8_code/data/StudentDataBase.java
+++ src/java_8_code/data/StudentDataBase.java
@@ -31,7 +31,7 @@ public class StudentDataBase {
          * 2nd grade students
          */
         Student student1 = new Student("Adam",2,3.6, "male",10,Arrays.asList("swimming", "basketball","volleyball"));
-        Student student2 = new Student("Jenny",2,3.8,"female", 11,Arrays.asList("swimming", "gymnastics","soccer"));
+        Student student2 = new Student("Jenny",2,1.8,"female", 11,Arrays.asList("swimming", "gymnastics","soccer"));
         /**
          * 3rd grade students
          */
